79437c2a3064805e22ff744c2693d07ceee6e810
Anna Khamsamran <akhamsamran1@cnm.edu>
2018-06-06 11:24:24 -0600

    added accessor and mutator methods for categoryName

--- php/classes/category.php
+++ php/classes/category.php
@@ -78,7 +78,35 @@ class Profile implements \JsonSerializable {
 		//convert and store the category id
 		$this->categoryId = $uuid;
 	}
-
+	/**
+	 * accessor method for category name
+	 * @return string value of category name
+	 **/
+	public function getCategoryName() : string {
+		return($this->categoryName);
+	}
+	/**
+	 * mutator method for category name
+	 *
+	 * @param string $newCategoryName new value of category name
+	 * @throws \InvalidArgumentException if $newCategoryName is not a string or insecure
+	 * @throws \RangeException if $newCategoryName is >64 characters
+	 * @throws \TypeError if $newCategoryName is not a string
+	 **/
+	public function setCategoryName(string $newCategoryName) : void {
+		//verify the category name is secure
+		$newCategoryName = trim($newCategoryName);
+		$newCategoryName = filter_var($newCategoryName, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty($newCategoryName) === true) {
+			throw(new \InvalidArgumentException("category name is empty or insecure"));
+		}
+		//verify the category name will fit in the database
+		if(strlen($newCategoryName) > 64) {
+			throw(new \RangeException("category name too long"));
+		}
+		//store the category name
+		$this->categoryName = $newCategoryName;
+	}
 
 
 
